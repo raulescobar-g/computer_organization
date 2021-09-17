fd5392337e87d450117b07f82ae1bf5a137be011
raulescobar-g <raulescobar_g@tamu.edu>
2021-09-16 19:05:29 -0500

    pa1 mods

--- PA1/BuddyAllocator.cpp
+++ PA1/BuddyAllocator.cpp
@@ -1,26 +1,106 @@
 #include "BuddyAllocator.h"
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-BuddyAllocator::BuddyAllocator (int _basic_block_size, int _total_memory_length){
+BlockHeader* BuddyAllocator::getbuddy (BlockHeader * addr){
+	return (BlockHeader*) ((char*)addr + addr->block_size); 
+}
+
+bool BuddyAllocator::arebuddies (BlockHeader* block1, BlockHeader* block2){
+	if (block1 == getbuddy(block2)){
+		return true;
+	}
+	else if (block2 == getbuddy(block1)){
+		return true;
+	} 
+	else{
+		return false;
+	}
+}
+
+BlockHeader* BuddyAllocator::merge(BlockHeader* block1, BlockHeader* block2){
+	int index = (int) log2(ceil( (double) (block1->block_size) / basic_block_size)); // this might be wrong lmao
 	
+	FreeList[index].remove(block1);
+	FreeList[index].remove(block2);
+
+	block1->block_size *= 2;
+	FreeList[index+1].insert(block1);
+
+	return block1;
+
+}
+
+BlockHeader* BuddyAllocator::split(BlockHeader* b){
+	int bs = b->block_size;
+	b->block_size /= 2;
+	b->next = nullptr;
+
+	BlockHeader* sh = getbuddy(b);
+	sh->next = nullptr;
+	sh->block_size = b->block_size;
+	//BlockHeader* temp = new (sh) BlockHeader (b->block_size);
+	return sh;
+}
+
+BuddyAllocator::BuddyAllocator (int _basic_block_size, int _total_memory_length){
+	total_memory_size = _total_memory_length; 
+	basic_block_size = _basic_block_size;
+
+	start = new char[total_memory_size];
+
+	int l = log2(total_memory_size / basic_block_size);
+
+	for (int i = 0; i < l; i++){
+		FreeList.push_back(LinkedList());
+	}
+	FreeList.push_back(LinkedList((BlockHeader*) start)); // last element in LL
+	BlockHeader* h = new (start) BlockHeader(total_memory_size);
 }
 
 BuddyAllocator::~BuddyAllocator (){
-	
+	delete [] start;
 }
 
-char* BuddyAllocator::alloc(int _length) {
-  /* This preliminary implementation simply hands the call over the 
-     the C standard library! 
-     Of course this needs to be replaced by your implementation.
-  */
-  return new char [_length];
+char* BuddyAllocator::alloc(int _length) { 
+  	int x = _length + sizeof(BlockHeader);
+	int index = (int) log2(ceil( (double) x / basic_block_size));
+
+	int blockSizeReturn = (1 << index) * basic_block_size;
+
+	if (FreeList[index].head != nullptr){
+		BlockHeader* b = FreeList[index].remove();
+		return (char*) (b+1);
+	}
+	else {
+		int const_index = index;
+		for (;index < FreeList.size() ; index++){
+			if (FreeList[index].head != nullptr){
+				break;
+			}
+		}
+		if (index >= FreeList.size()){
+			return nullptr;
+		}
+
+		while (index > const_index){
+			BlockHeader* b = FreeList[index].remove();
+			BlockHeader* shb = split(b);
+			--index;
+			FreeList[index].insert(b);
+			FreeList[index].insert(shb);
+		}
+		return (char*) (FreeList[index].remove()+1);
+	}
 }
 
 int BuddyAllocator::free(char* _a) {
-  /* Same here! */
-  delete _a;
+  //shift _a from the writable memory to the blockheader with metadata
+	//check if block to be deleted is in FreeList
+	//add block to free list
+	//if buddy is free merge recursively 
+
   return 0;
 }
 
--- PA1/Main.cpp
+++ PA1/Main.cpp
@@ -1,10 +1,10 @@
-//#include "Ackerman.h"
-//#include "BuddyAllocator.h"
+#include "Ackerman.h"
+#include "BuddyAllocator.h"
 #include <unistd.h>
 #include <iostream>
 using namespace std;
 
-/* void easytest(BuddyAllocator* ba){
+void easytest(BuddyAllocator* ba){
 	// be creative here
 	// know what to expect after every allocation/deallocation cycle
 
@@ -18,45 +18,45 @@ using namespace std;
 	ba->free (mem); // give back the memory you just allocated
 	ba->printlist(); // shouldn't the list now look like as in the beginning
 
-} */
+} 
 
 int main(int argc, char ** argv) {
 
 	int c, 
-		blocksize = 128,
-		memsize = 512 * 1024;
+		basic_block_size = 128,
+		memory_length = 512 * 1024;
 
   	while (((c = getopt(argc,argv,"b:s:")) != -1)) {
 		switch (c){
 			case 'b':
-				blocksize = atoi(optarg);
+				basic_block_size = atoi(optarg);
 				break;
 			case 's':
-				memsize = atoi(optarg);
+				memory_length = atoi(optarg);
 				break;
 			case '?':
 				std::cout<<"Only -b or -s flag are valid"<<std::endl;
-				return 0;
+				return 1;
 			default:
 				std::cout<<"How did you even get to this error bro?"<<std::endl;
 		}
   	}
 
-	std::cout<<"BlockSize: "<<blocksize<<std::endl<<"MemorySize: "<<memsize<<std::endl;
+	// these were defaults 
+  	//int basic_block_size = 128, 
+	//  	memory_length = 128 * 1024 * 1024;
 
-  	// int basic_block_size = 128, memory_length = 128 * 1024 * 1024;
+	// create memory manager
+	BuddyAllocator * allocator = new BuddyAllocator(basic_block_size, memory_length);
 
-	// // create memory manager
-	// BuddyAllocator * allocator = new BuddyAllocator(basic_block_size, memory_length);
-
-	// // the following won't print anything until you start using FreeList and replace the "new" with your own implementation
-	// easytest (allocator);
+	// the following won't print anything until you start using FreeList and replace the "new" with your own implementation
+	easytest (allocator);
 
 	
-	// // stress-test the memory manager, do this only after you are done with small test cases
-	// Ackerman* am = new Ackerman ();
-	// am->test(allocator); // this is the full-fledged test. 
+	// stress-test the memory manager, do this only after you are done with small test cases
+	Ackerman* am = new Ackerman ();
+	am->test(allocator); // this is the full-fledged test. 
 	
-	// // destroy memory manager
-	// delete allocator;
+	// destroy memory manager
+	delete allocator;
 }
